8b600c80c4f514b69eab5f1cc8e692e370e6e118
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-22 14:14:17 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Meu nome em uma caixa!");
+Console.WriteLine("+------------+");
+Console.WriteLine("|   Ramon    |");
+Console.WriteLine("+------------+");
